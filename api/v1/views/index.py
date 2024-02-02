dba9930ef9a0cefb7cb0c056d9ca0e49856d6df1
Seth Ketsi <christambassador12@gmail.com>
2024-02-02 14:31:00 +0000

    Return status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,11 @@
 """Views for the index"""
 
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status"""
-    return jsonify({"status": "OK"})
+    return {
+        "status": "OK",
+    }
