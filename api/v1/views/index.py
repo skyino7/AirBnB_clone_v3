e079eaaec91dc9af80873b6a7a248dbafd3f76ff
Seth Ketsi <christambassador12@gmail.com>
2024-02-02 15:46:32 +0000

    Created an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Views for the index"""
 
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -9,4 +10,16 @@ def status():
     """Return status"""
     return {
         "status": "OK",
+    }
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return stats"""
+    return {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
     }
