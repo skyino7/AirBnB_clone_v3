44d9e30225c16cc3fd0f1e24646762be8ac2d495
Seth Ketsi <christambassador12@gmail.com>
2024-02-04 20:11:34 +0000

    link between Place objects and Amenity objects that handles all default RESTFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,3 +11,5 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+from api.v1.views.places_amenities import *
