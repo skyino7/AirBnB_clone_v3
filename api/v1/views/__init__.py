1b421b3c8cae46fe02fcc1d5f862bc8bf1308049
Seth Ketsi <christambassador12@gmail.com>
2024-02-04 21:21:05 +0000

    try again

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,5 +11,4 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
 from api.v1.views.places_amenities import *
