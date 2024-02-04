a841ed39d754a26f736ee5d2adad03775a79490c
Seth Ketsi <christambassador12@gmail.com>
2024-02-04 21:00:04 +0000

    revert

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,18 +76,13 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """
-        returns object based on it's class and id
-        None if not found
-        Args:
-            id (int): id of the class instance
-            cls (obj): class object_
-        """
-        if cls in classes.values() and id and type(id) is str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
-                    return value
+        """retrieve one object"""
+        if cls is None or id is None:
+            return None
+        objs = self.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
         return None
 
     def count(self, cls=None):
